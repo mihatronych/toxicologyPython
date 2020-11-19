7982fb8543a26e17272eb93e1767f2a1e56698d8
ismakova <ismakova.dayana@mail.ru>
2020-11-19 14:12:22 +0500

    добавлен простой парсер для групп (берет 100 постов и у каждого поста по 100 комментариев) комментарии занесены в файл vk_comments.csv (около 12к)

--- vk_parser/parser.py
+++ vk_parser/parser.py
@@ -1,23 +1,37 @@
-import time
 import vk_methods
-import vk
+import pandas as pd
+import re
 
 # ----- id токсичных групп -----
 # degradination
 # kpopanti
 # antidolboebi
+# helisexual
+#
 # ------------------------------
 access_token = ''
 
-def get_group_comments(group_id):
-    while True:
-        post_id_list = vk_methods.get_posts_by_domain(group_id, 1, access_token)
-        print(post_id_list)
-        # a = str(post_id_list['items'][0]['id'])
-        # comm = vk_methods.get_posts_comment(group_id, a, access_token, 1)
-        # com_text = comm['items'][0]['text']
-        # print(com_text)
-        # time.sleep(5)
+
+def get_group_comments(domain):
+    post_id_list = vk_methods.get_posts_by_domain(domain, 100, access_token)
+    group_id = post_id_list['items'][0]['owner_id']
+    com_texts = []
+    for i in range(len(post_id_list['items'])):
+        post_id = str(post_id_list['items'][i]['id'])
+        comments = vk_methods.get_posts_comment(group_id, post_id, access_token, 100)
+        for j in range(len(comments['items'])):
+            if 'text' in comments['items'][j]:
+                com_texts.append(re.sub('^\s+|\n|\r|\s+$', '', comments['items'][j]['text']))
+
+    write_csv(com_texts)
+
+
+def write_csv(comments):
+    columns = ['comment']
+    df = pd.DataFrame(comments, columns=columns)
+    df.to_csv(r'vk_comments.csv', mode='a', header=True, index=False)
+
 
 if __name__ == '__main__':
-    get_group_comments("degradination")
+    group_domain = ''
+    # get_group_comments(group_domain)
--- vk_parser/vk_methods.py
+++ vk_parser/vk_methods.py
@@ -4,7 +4,7 @@ import vk
 def get_user(user_id, access_token):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.users.get(user_id=user_id, fields="photo_200, can_see_all_posts")
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -13,7 +13,7 @@ def get_user(user_id, access_token):
 def get_posts(id, count, access_token):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.wall.get(owner_id=id,  count=str(count), filter="all")
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -22,7 +22,7 @@ def get_posts(id, count, access_token):
 def get_posts_by_domain(domain, count, access_token):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.wall.get(domain=domain,  count=str(count), filter="all")
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -31,7 +31,7 @@ def get_posts_by_domain(domain, count, access_token):
 def get_posts_comment(id, post_id, access_token, count=10):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.wall.getComments(owner_id=id, post_id=post_id, count=str(count), sort="asc", preview_length=0)
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -40,7 +40,7 @@ def get_posts_comment(id, post_id, access_token, count=10):
 def get_comment_comments(id, post_id, comment_id, access_token, count=10):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.wall.getComments(owner_id=id, comment_id=comment_id, post_id=post_id, count=str(count), sort="asc", preview_length=0)
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -49,7 +49,7 @@ def get_comment_comments(id, post_id, comment_id, access_token, count=10):
 def get_users_followers(user_id, access_token, count=100):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.users.getFollowers(user_id=user_id, count=count, fields="photo_200, is_friend, wall_comments")
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -58,7 +58,7 @@ def get_users_followers(user_id, access_token, count=100):
 def get_users_subscriptions(user_id, access_token, count=100):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.users.getSubscriptions(user_id=user_id, count=count)
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -67,7 +67,7 @@ def get_users_subscriptions(user_id, access_token, count=100):
 def get_group(group_id, access_token):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.groups.getById(group_id=group_id, fields="description, can_see_all_posts")
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -76,7 +76,7 @@ def get_group(group_id, access_token):
 def get_groups_members(group_id, access_token, count=1000):
     try:
         session = vk.Session(access_token=access_token)
-        vk_api = vk.API(session, v="5.92")
+        vk_api = vk.API(session, v="5.126")
         return vk_api.groups.getMembers(group_id=group_id, sort="id_asc", count=count)
     except vk.exceptions.VkAPIError as e:
         return e.message
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     access_token = ''
     # print(get_user(1, access_token))
     # print(get_posts(1, 20, access_token))
-    print(get_posts_by_domain("degradination", 20, access_token))
+    # print(get_posts_by_domain("degradination", 20, access_token))
     # print(get_posts_comment(1, 2442097, access_token, 20))
     # print(get_comment_comments(1, 2442097, 2442108, access_token, 20))
     # print(get_users_followers(1, access_token, 100))
