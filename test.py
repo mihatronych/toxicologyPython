5599f9354ec535b6badfa1f2fd7f45f1fa66aef2
mihatronych <dolgushin.mikhail131200@yandex.ru>
2020-11-30 15:21:02 +0500

    Ru classificator
    Теперь и русскую родную токсичность находит, но точность 70%

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ from sklearn.svm import SVC, LinearSVC, NuSVC
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 import pickle
 import zipfile
+from nltk.stem.snowball import SnowballStemmer
 
 class PreProcessSome:
     def __init__(self):
@@ -30,6 +31,26 @@ class PreProcessSome:
         doc = word_tokenize(doc)  # remove repeated characters (helloooooooo into hello)
         return [word for word in doc if word not in self._stopwords]
 
+class PreProcessSomeRusnya:
+    def __init__(self):
+        self._stopwords = set(stopwords.words('russian') + list(punctuation) + list('``'))
+
+    def processText(self, list_of_docs):
+        processedDocs = []
+        id = 0
+        for doc in list_of_docs:
+            processedDocs.append([id, self.processDoc(doc[0]), doc[1]])
+            id += 1
+        return processedDocs
+
+    def processDoc(self, doc):
+        stemmer = SnowballStemmer("russian")
+        doc = doc.lower()  # convert text to lower-case
+        doc = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', doc)  # remove URLs
+        doc = re.sub('@[^\s]+', 'AT_USER', doc)  # remove usernames
+        doc = re.sub(r'#([^\s]+)', r'\1', doc)  # remove the # in #hashtag
+        doc = word_tokenize(doc)  # remove repeated characters (helloooooooo into hello)
+        return [stemmer.stem(word) for word in doc if word not in self._stopwords]
 
 def buildVocabulary(preprocessedTrainingData):
     all_words = []
@@ -71,7 +92,7 @@ def pretrainNB():
     f.close()
     print("Accuracy:", acc)
 
-def pretrainSVC():
+def pretrainSVCeng():
     path = 'input/'
     train_data_file = f'{path}train.csv'
     trainx = pd.read_csv(train_data_file)
@@ -82,7 +103,6 @@ def pretrainSVC():
     tutu = []
     for i in ppr:
         tutu.append((extract_features(i[1], wf), "toxic" if i[2] == 1 else "untoxic"))
-    print(tutu)
     random.shuffle(tutu)
     train_x = tutu[:1800]
     SVC_classifier = SklearnClassifier(SVC(kernel='linear',probability=True))
@@ -96,6 +116,33 @@ def pretrainSVC():
     with zipfile.ZipFile(f'{path}svc.zip', mode='w',compression=zipfile.ZIP_DEFLATED) as zf:
         zf.write(f'{path}my_svc_classifier.pickle', arcname="my_svc_classifier.pickle")
     os.remove(f'{path}my_svc_classifier.pickle')
+    print("Accuracy:", acc)
+
+
+def pretrainSVCru():
+    path = 'input/'
+    train_data_file = f'{path}toxic_labeled_comments.csv'
+    trainx = pd.read_csv(train_data_file)
+    train = trainx.values[:3000]
+    ppr = PreProcessSomeRusnya()
+    ppr = ppr.processText(train)
+    wf = buildVocabulary(ppr)
+    tutu = []
+    for i in ppr:
+        tutu.append((extract_features(i[1], wf), "toxic" if i[2] == 1 else "untoxic"))
+    random.shuffle(tutu)
+    train_x = tutu[:2400]
+    SVC_classifier = SklearnClassifier(SVC(kernel='linear',probability=True))
+    SVC_classifier.train(train_x)
+    test_x = tutu[2400:]
+    acc = nltk.classify.accuracy(SVC_classifier, test_x)
+    path = "output/"
+    f = open(f'{path}my_ru_svc_classifier.pickle', 'wb')
+    pickle.dump(SVC_classifier, f)
+    f.close()
+    with zipfile.ZipFile(f'{path}svc_ru.zip', mode='w',compression=zipfile.ZIP_DEFLATED) as zf:
+        zf.write(f'{path}my_ru_svc_classifier.pickle', arcname="my_ru_svc_classifier.pickle")
+    os.remove(f'{path}my_ru_svc_classifier.pickle')
     print("Accuracy:", acc)
 
 def classify_with_modelling_NB(message):
@@ -121,7 +168,8 @@ def classify_with_modelling_NB(message):
                        + str(r_prob) + " Toxic\n\r " +
                        str(1 - r_prob) + " Untoxic\n\r")
 
-def classify_with_modelling_SVC(message):
+# Сделать надо будет потом в одну функцию с ру версией
+def classify_with_modelling_SVCeng(message):
     path = 'input/'
     train_data_file = f'{path}train.csv'
     trainx = pd.read_csv(train_data_file)
@@ -133,6 +181,28 @@ def classify_with_modelling_SVC(message):
     path = "output/"
     archive = zipfile.ZipFile(f'{path}svc.zip', 'r')
     f = archive.open("my_svc_classifier.pickle")
+    model = pickle.load(f)
+    t_features = extract_features(ppr_message, wf)
+    r_prob = 0.0
+    for i in range(5):
+        r_prob += model.prob_classify(t_features).prob("toxic")
+    r_prob /= 5
+    return str("MESSAGE\r\n"+message + " : " +"\n\rSVC classification RESULT is\n\r "
+                       + str(r_prob) + " Toxic\n\r " +
+                       str(1 - r_prob) + " Untoxic\n\r")
+
+def classify_with_modelling_SVCru(message):
+    path = 'input/'
+    train_data_file = f'{path}toxic_labeled_comments.csv'
+    trainx = pd.read_csv(train_data_file)
+    train = trainx.values[:3000]
+    ppr = PreProcessSomeRusnya()
+    ppr_vocab = ppr.processText(train)
+    ppr_message = ppr.processDoc(message)
+    wf = buildVocabulary(ppr_vocab)
+    path = "output/"
+    archive = zipfile.ZipFile(f'{path}svc_ru.zip', 'r')
+    f = archive.open("my_ru_svc_classifier.pickle")
     model = pickle.load(f)
     t_features = extract_features(ppr_message, wf)
     r_prob = 0.0
@@ -171,7 +241,7 @@ def classify_many_with_modelling_NB(messages):
                        str(1 - r_prob) + " Untoxic\n\r"))
     return result
 
-def classify_many_with_modelling_SVC(messages):
+def classify_many_with_modelling_SVCeng(messages):
     path = 'input/'
     train_data_file = f'{path}train.csv'
     trainx = pd.read_csv(train_data_file)
@@ -185,6 +255,34 @@ def classify_many_with_modelling_SVC(messages):
     path = "output/"
     archive = zipfile.ZipFile(f'{path}svc.zip', 'r')
     f = archive.open("my_svc_classifier.pickle")
+    model = pickle.load(f)
+    result = []
+    for k in range(len(messages)):
+        t_features = extract_features(ppr_messages[k], wf)
+        r_prob = 0.0
+        for i in range(5):
+            r_prob += model.prob_classify(t_features).prob("toxic")
+        r_prob /= 5
+
+        result.append(("MESSAGE\r\n"+messages[k] + " : " +"\n\rSVC classification RESULT is\n\r ")
+                       + str(r_prob) + " Toxic\n\r " +
+                       str(1 - r_prob) + " Untoxic\n\r")
+    return result
+
+def classify_many_with_modelling_SVCru(messages):
+    path = 'input/'
+    train_data_file = f'{path}toxic_labeled_comments.csv'
+    trainx = pd.read_csv(train_data_file)
+    train = trainx.values[:3000]
+    ppr = PreProcessSomeRusnya()
+    ppred = ppr.processText(train)
+    ppr_messages = []
+    for message in messages:
+        ppr_messages.append(ppr.processDoc(message))
+    wf = buildVocabulary(ppred)
+    path = "output/"
+    archive = zipfile.ZipFile(f'{path}svc_ru.zip', 'r')
+    f = archive.open("my_ru_svc_classifier.pickle")
     model = pickle.load(f)
     result = []
     for k in range(len(messages)):
@@ -228,33 +326,38 @@ if __name__ == '__main__':
 
     path = 'input/'
 
-    train_data_file = f'{path}train.csv'
+    #train_data_file = f'{path}train.csv'
+    train_data_file = f'{path}toxic_labeled_comments.csv'
     trainx = pd.read_csv(train_data_file)
     #pretrainNB()
-    #pretrainSVC()
+    #pretrainSVCeng() #90% точность почти
+    #pretrainSVCru() #кое-как 70% точность, це фигово, но в принципе угадывает
     test = trainx.values[3150:3200]
     messages = []
-    #for message in test:
+    for message in test:
+        messages.append(message[0])
+
     #    messages.append(message[1])
-    #[print(i) for i in classify_many_with_modelling_NB(messages)]
-    #[print(i) for i in classify_many_with_modelling_SVC(messages)]
+    # [print(i) for i in classify_many_with_modelling_NB(messages)]
+    # [print(i) for i in classify_many_with_modelling_SVCeng(messages)] # для английской версии
+    # [print(i) for i in classify_many_with_modelling_SVCru(messages)] # для русской версии
     #print(messages[0])
     #print(classify_with_modelling_NB(messages[0]))
-    #print(classify_with_modelling_SVC(messages[0]))
+    #print(classify_with_modelling_SVCeng(messages[0]))
 
-    csv_path = "vk_parser/vk_comments.csv"
-    data = []
-    with open(csv_path, "r", encoding='utf-8') as f_obj:
-        data = csv_reader(f_obj)
-        data[0] = "comment#toxic"
-        data = [el.split("#") for el in data]
+    #csv_path = "vk_parser/vk_comments.csv"
+    #data = []
+    #with open(csv_path, "r", encoding='utf-8') as f_obj:
+    #    data = csv_reader(f_obj)
+    #    data[0] = "comment#toxic"
+    #    data = [el.split("#") for el in data]
 
-    my_list = []
-    fieldnames = data[0]
-    for values in data[1:]:
-        inner_dict = dict(zip(fieldnames, values))
-        my_list.append(inner_dict)
+    #my_list = []
+    #fieldnames = data[0]
+    #for values in data[1:]:
+    #    inner_dict = dict(zip(fieldnames, values))
+    #    my_list.append(inner_dict)
 
-    path = "vk_parser/vk_comments_DS.csv"
-    csv_dict_writer(path, fieldnames, my_list)
+    #path = "vk_parser/vk_comments_DS.csv"
+    #csv_dict_writer(path, fieldnames, my_list)
 
