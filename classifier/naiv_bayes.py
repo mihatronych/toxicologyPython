f68ae91517a3167b0fe14e72d44f41dccfa53471
ISMAKOVA <48486359+ismakova@users.noreply.github.com>
2020-12-07 19:05:33 +0500

    добавлен лемматизированный датасет

--- classifier/naiv_bayes.py
+++ classifier/naiv_bayes.py
@@ -12,19 +12,17 @@ from nltk.stem.snowball import SnowballStemmer
 import pickle
 from sklearn.metrics import accuracy_score
 from pymystem3 import Mystem
-
+import pymorphy2
 
 def preprocessing_data(text):
     stop_words = set(stopwords.words('russian') + list(punctuation)) #список стоп-слов
     stemmer = SnowballStemmer("russian")
-    # m = Mystem() #для лемматизации
+    #morph = pymorphy2.MorphAnalyzer() #для лемматизации
     text = text.lower() #приведение слов к строчным
     text = re.sub('[^а-яА-Я]', ' ', text, flags=re.MULTILINE)#удаляем знаки пунктуации
     tokens = word_tokenize(text) #разделяем слова на отдельные токены
     text = [word for word in tokens if word not in stop_words] #удаляем стоп-слова
     text = [stemmer.stem(word) for word in text] #производим стемминг
-    # text = ' '.join(text)
-    # text = m.lemmatize(text)
     text = ' '.join(text)
     return text
 
@@ -40,12 +38,16 @@ def read_pickle(name):
 
 
 def training_data(input_data):
-    train_db = pd.read_csv(input_data) #получение датасета
-    # train_db = train_db[:4000]
-    print("Препроцессинг")
-    train_db['comment'] = train_db['comment'].map(preprocessing_data) #подготовка данных к обработке
+    #######################################################
+    # train_db = pd.read_csv(input_data) #получение датасета
+    # # train_db = train_db[:4000]
+    # print("Препроцессинг")
+    # train_db['comment'] = train_db['comment'].map(preprocessing_data) #подготовка данных к обработке
+    #######################################################
+
+    train_db = pd.read_csv(input_data)
     #разделение на тренировочную и тестовую выборку
-    X_train, X_test, y_train, y_test = train_test_split(train_db['comment'], train_db['toxic'], test_size=0.33,
+    X_train, X_test, y_train, y_test = train_test_split(train_db['comment'].values.astype('U'), train_db['toxic'], test_size=0.33,
                                                         random_state=42)
     #формирование словаря
     count_vect = CountVectorizer()
@@ -86,10 +88,18 @@ def classifier(messages):
     return zip(messages, predicted)
 
 
-if __name__ == '__main__':
-    #input_data = 'labeled_ru_ds.csv'
-    # training_data(input_data)
+def save_clear_data(input_data):
+    train_db = pd.read_csv(input_data)
+    train_db['comment'] = train_db['comment'].map(preprocessing_data)
+    columns = ['comment', 'toxic']
+    df = pd.DataFrame(train_db, columns=columns)
+    df.to_csv(r'clear_data.csv', mode='a', header=True, index=False)
 
+if __name__ == '__main__':
+    # input_data = 'labeled_ru_ds.csv' #для новой прогонки
+    # input_data = 'clear_data.csv' #лемматизированный датасет
+    # training_data(input_data)
+    # save_clear_data(input_data)
     messages = ["Верблюдов-то за что? Дебилы, бл...",
                 "Хохлы, это отдушина затюканого россиянина, мол, вон, а у хохлов еще хуже. Если бы хохлов не было, кисель их бы придумал.",
                 "Какой чудесный день!",
